fd11bc661aa58862fdd6bc7a31ffd55e288411b3
Skaps664 <msudaisk664@gmail.com.com>
2025-04-06 14:37:02 +0500

    button created for ImageUplaod

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ImageUpload from './ImageUpload'
+import ImagePreview from './ImagePreview'
+
+
+const Home = () => {
+  return (
+    <div>
+
+        <ImageUpload />
+        <ImagePreview />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageUpload = () => {
+  return (
+    <div className='bg-gray-200 rounded-2xl p-6'>
+        <label htmlFor="fileInput" className='block p-2 w-full border border-dashed rounded-lg hover:border-blue-500 transition-all'> Click to uplaod</label>
+    </div>
+  )
+}
+
+export default ImageUpload
