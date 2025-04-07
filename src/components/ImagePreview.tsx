971fefc6d81647abf377e9c882fb039d2376d85d
Skaps664 <msudaisk664@gmail.com.com>
2025-04-07 15:27:39 +0500

    UI completed without logic

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const ImagePreview = () => {
   return (
-    <div>ImagePreview</div>
+<div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
+            {/* Original Image */}
+            <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+                <h2 className="font-semibold text-center bg-gray-800 text-white py-2">
+                    Original Image
+                </h2>
+
+                    <img
+                        src=""
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                    <div className="flex items-center justify-center h-80 bg-gray-200">
+                        No Image Selected
+                    </div>
+            </div>
+
+            {/* Enhanced Image Image */}
+            <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+                <h2 className=" font-semibold text-center bg-blue-800 text-white py-2">
+                    Enhanced Image
+                </h2>
+
+                    <img
+                        src=""
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+
+                    <div className="flex items-center justify-center h-80 bg-gray-200">
+                        No Enhanced Image
+                    </div>
+            </div>
+        </div>
   )
 }
 
--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const ImageUpload = () => {
   return (
-    <div className='bg-gray-200 rounded-2xl p-6'>
-        <label htmlFor="fileInput" className='block p-2 w-full border border-dashed rounded-lg hover:border-blue-500 transition-all'> Click to uplaod</label>
-    </div>
+    <div className="bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl">
+            <label
+                htmlFor="fileInput"
+                className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6 text-center hover:border-blue-500 transition-all"
+            >
+                <input
+                    type="file"
+                    id="fileInput"
+                    className="hidden"
+                />
+                <span className="text-lg font-medium text-gray-600">
+                    Click and drag to upload your image
+                </span>
+            </label>
+        </div>
+
   )
 }
 
